1c71f7301bd2f92148a808eb99e94bd818e9d176
tandoh22 <tandohsamuel4u@gmail.com>
2025-07-10 14:09:31 +0000

    new new

--- Category.java
+++ Category.java
@@ -1,7 +1,13 @@
 import java.util.HashSet;
+import java.util.List;
+
 
 public class Category {
     private HashSet<String> categories = new HashSet<>();
+
+    public List<String> getCategories() {
+        return new java.util.ArrayList<>(categories);
+    }
 
     public boolean addCategory(String category) {
         return categories.add(category);
