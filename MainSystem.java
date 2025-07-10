a3c22dbe4e51ade6198dc2b70f20212d1e270fb9
tandoh22 <tandohsamuel4u@gmail.com>
2025-07-10 11:19:45 +0000

    Refactor MainSystem and MinHeap classes for improved structure and functionality

--- MainSystem.java
+++ MainSystem.java
@@ -1,3 +1,11 @@
+import java.io.*;
+import java.util.*;
+
 public class MainSystem {
+    static Scanner scanner = new Scanner(System.in);
+    static HashMap<String, Expendituree> expenditureMap = new HashMap<>();
+    static LinkedList<Expendituree> expenditureList = new LinkedList<>();
+    static HashMap<String, BankAccount> bankAccounts = new HashMap<>();
+    static Category categoryManager = new Category();
     
 }
--- MinHeap.java
+++ MinHeap.java
@@ -1,9 +1,26 @@
-public Class MinHeap {
+public class MinHeap {
     private BankAccount[] heap;
     private int size;
 
     public MinHeap(int capacity) {
         heap = new BankAccount[capacity];
         size = 0;
+    }
+
+    public void insert(BankAccount acc) {
+        heap[size] = acc;
+        int current = size;
+        size++;
+
+        while (current > 0 && heap[current].balance < heap[(current - 1) / 2].balance) {
+            BankAccount temp = heap[current];
+            heap[current] = heap[(current - 1) / 2];
+            heap[(current - 1) / 2] = temp;
+            current = (current - 1) / 2;
+        }
+    }
+
+    public BankAccount getmin() {
+        return heap[0];
     }
 }
