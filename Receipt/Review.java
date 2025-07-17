dcb67cdcbfb1f997a8c347e5bd793f1b6449595d
tandoh22 <tandohsamuel4u@gmail.com>
2025-07-17 08:01:57 +0000

    couple of changes

--- Receipt/Review.java
+++ Receipt/Review.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class Review {
     public Stack<String> reviewStack = new Stack<>();
-    public void reviewReceipt(String receipt) {
-        reviewStack.push(receipt);
+    public void reviewReceipt(String receiptQueue) {
+        reviewStack.push(receiptQueue);
     }
 }
