6a8a0a84d5b9a8e478ad243f01eb80cb4cdac11c
tandoh22 <tandohsamuel4u@gmail.com>
2025-07-31 11:13:50 +0000

    Stack implementation

--- Stack.java
+++ Stack.java
@@ -1,3 +1,38 @@
-public class Stack {
-    
+public class Stack<T> {
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node(T data) { this.data = data; }
+    }
+
+    private Node<T> top;
+    private int size;
+
+    public void push(T element) {
+        Node<T> newNode = new Node<>(element);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() {
+        if (top == null) throw new IllegalStateException("Stack is empty");
+        T value = top.data;
+        top = top.next;
+        size--;
+        return value;
+    }
+
+    public T peek() {
+        if (top == null) throw new IllegalStateException("Stack is empty");
+        return top.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 }
