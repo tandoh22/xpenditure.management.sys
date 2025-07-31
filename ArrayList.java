186b6e7f9dc3ba057989564ad1cbea4accb45819
tandoh22 <tandohsamuel4u@gmail.com>
2025-07-31 10:50:40 +0000

    arraylist implementation

--- ArrayList.java
+++ ArrayList.java
@@ -1,3 +1,46 @@
-public class ArrayList {
-    
+public class ArrayList<T> {
+    private Object[] data;
+    private int size;
+
+    public ArrayList() {
+        data = new Object[10]; // initial capacity
+        size = 0;
+    }
+
+    public void add(T element) {
+        if (size == data.length) {
+            resize();
+        }
+        data[size++] = element;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) data[index];
+    }
+    @SuppressWarnings("unchecked")
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        T removed = (T) data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[--size] = null;
+        return removed;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void resize() {
+        Object[] newData = new Object[data.length * 2];
+        System.arraycopy(data, 0, newData, 0, data.length);
+        data = newData;
+    }
 }
