e0a4ae72409004677105630721cdcac846322600
tandoh22 <tandohsamuel4u@gmail.com>
2025-07-31 11:10:54 +0000

    Queue implementation

--- Queue.java
+++ Queue.java
@@ -1,3 +1,44 @@
-public class Queue {
-    
+public class Queue<T> {
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+        Node(T data) { this.data = data; }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
+
+    public void add(T element) { // enqueue
+        Node<T> newNode = new Node<>(element);
+        if (tail == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public T remove() { // dequeue
+        if (head == null) throw new IllegalStateException("Queue is empty");
+        T value = head.data;
+        head = head.next;
+        if (head == null) tail = null;
+        size--;
+        return value;
+    }
+
+    public T peek() {
+        if (head == null) throw new IllegalStateException("Queue is empty");
+        return head.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 }
